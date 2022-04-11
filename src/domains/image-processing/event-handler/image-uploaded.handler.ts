e7695f41cb390fd4ca1dff230990c2ea2253d788
Sebbb1 <guyonnaud.sebastien@gmail.com>
2022-04-11 21:53:48 +0200

    Create image resizing

--- src/domains/image-processing/event-handler/image-uploaded.handler.ts
+++ src/domains/image-processing/event-handler/image-uploaded.handler.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import Jimp from 'jimp';
 import { updateImageProcessing } from '../database/image/image.repository';
 
 export type ImageUploaded = {
@@ -24,8 +25,46 @@ export const handleImageUploaded = async (images: ImageUploaded[]) => {
 					resolve(undefined);
 				});
 			});
-			
-			await updateImageProcessing(id, false);
 		}),
-	)
+	);
+
+	images.forEach(async ({ id }) => {
+		const imagePath = `files/images/full/original/${id}.jpg`;
+		const jimpImage = await Jimp.read(imagePath);
+
+		const fullResolutions = [400, 1080];
+		const thumbnailResolutions = [400, 80];
+
+		for (const height of fullResolutions) {
+			console.log(`Creating full ${height} of ${id}`);
+
+			const resized = jimpImage
+				.clone()
+				.resize(Jimp.AUTO, height);
+
+			await resized.writeAsync(`files/images/full/${height}/${id}.jpg`);
+		}
+
+		for (const heightAndWidth of thumbnailResolutions) {
+			console.log(`Creating thumbnail ${heightAndWidth} of ${id}`);
+
+			const isLandscape = jimpImage.getHeight() < jimpImage.getWidth(); 
+
+			const resized = jimpImage
+				.clone()
+				.resize(
+					isLandscape ? Jimp.AUTO : heightAndWidth,
+					isLandscape ? heightAndWidth : Jimp.AUTO
+				);
+
+			const x = isLandscape ? (resized.getWidth() / 2) - (heightAndWidth / 2) : 0; 
+			const y = isLandscape ? 0 : (resized.getHeight() / 2) - (heightAndWidth / 2);
+
+			const cropped = resized.crop(x, y, heightAndWidth, heightAndWidth);
+
+			await cropped.writeAsync(`files/images/thumbnail/${heightAndWidth}/${id}.jpg`);
+		}
+
+		await updateImageProcessing(id, false);
+	});
 };
