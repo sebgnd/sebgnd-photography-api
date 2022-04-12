b6a5069df70eca1047f715d667a83f9031d9eb1e
Sebbb1 <guyonnaud.sebastien@gmail.com>
2022-04-12 21:00:48 +0200

    Fix whitespaces

--- src/database/entities/image.ts
+++ src/database/entities/image.ts
@@ -4,36 +4,36 @@ import { CATEGORY_SCHEMA_NAME, IMAGE_SCHEMA_NAME } from '../constant';
 import { OrmEntity } from '../types';
 
 interface Exif {
-  iso: number;
-  shutterSpeed: number;
-  aperture: number;
-  focalLength: number;
+	iso: number;
+	shutterSpeed: number;
+	aperture: number;
+	focalLength: number;
 }
 
 interface Image {
-  exif?: Exif;
-  processing: boolean,
-  category: Schema.Types.ObjectId,
+	exif?: Exif;
+	processing: boolean,
+	category: Schema.Types.ObjectId,
 }
 
 const imageSchema = new Schema<Image>({
-  exif: {
-    type: {
-      iso: Number,
-      shutterSpeed: String,
-      aperture: String,
-      focalLength: String,
-    },
-  },
-  category: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    ref: CATEGORY_SCHEMA_NAME,
-  },
-  processing: {
-	  type: Boolean,
-	  required: true,
-  }
+	exif: {
+		type: {
+			iso: Number,
+			shutterSpeed: String,
+			aperture: String,
+			focalLength: String,
+		},
+	},
+	category: {
+		type: Schema.Types.ObjectId,
+		required: true,
+		ref: CATEGORY_SCHEMA_NAME,
+	},
+	processing: {
+		type: Boolean,
+		required: true,
+	}
 }, { timestamps: true });
 
 export const ImageModel = mongoose.model(IMAGE_SCHEMA_NAME, imageSchema);
