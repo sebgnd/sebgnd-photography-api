4b743f863783841d7dc9a79cc91a48e00633d59b
Sebbb1 <guyonnaud.sebastien@gmail.com>
2022-07-24 17:07:25 +0200

    Set same site to none

--- src/domains/iam/transport/token-response.ts
+++ src/domains/iam/transport/token-response.ts
@@ -24,6 +24,7 @@ export const safelySendToken = (res: Response, tokens: TokenToSend) => {
 			seconds: refreshToken.ttl,
 		}),
 		secure: process.env.NODE_ENV === 'prod',
+		sameSite: 'none',
 		httpOnly: true,
 	});
 	res.status(200).json({
